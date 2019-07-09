b15d292ef717b602de2a3c71a7ac4b1d9512bea4
whiteCcinn <471113744@qq.com>
2019-07-09 17:38:24 +0800

    最长公共前缀【水平扫描法】

--- algorithm/lcp-1.c
+++ algorithm/lcp-1.c
@@ -4,16 +4,6 @@
 
 char *longestCommonPrefix(char **strs, int strSize)
 {
-
-  char *str = (char *)malloc(sizeof(char) * 100);
-
-  strcpy(str, strs[0]);
-
-  // 拿第8-10行代表和第13行比较，如果写法如下的话：
-  // char *str = strs[0];
-  // 全局数据区和栈区的字符串（也包括其他数据）有读取和写入的权限，而常量区的字符串（也包括其他数据）只有读取权限，没有写入权限。
-  // 所以直接指向的时候，第31行：str[j] = '\0';将会报段错误
-
   int i, j;
 
   if (strSize == 0)
@@ -21,14 +11,18 @@ char *longestCommonPrefix(char **strs, int strSize)
     return "";
   }
 
+  char *str = (char *)malloc(sizeof(char) * 1000);
+
+  strcpy(str, strs[0]);
+
   for (i = 1; i < strSize; i++)
   {
     j = 0;
     while (str[j] && strs[i][j] && str[j] == strs[i][j])
     {
-      j++; //避免数组溢出并判断下一个元素是否相同
+      j++;
     }
-    str[j] = '\0'; //字符串结束符只输出前面对应相同的
+    str[j] = '\0';
   }
 
   return str;
